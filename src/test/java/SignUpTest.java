12b0664d3fc94b3d2cb13ab2451251af5cf82799
RazmikMkhitaryan <razmik.mkhitaryan@picsart.com>
2021-06-07 20:41:49 +0400

    Examen , Login, sign up

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -1,10 +1,18 @@
 import pages.HomePage;
 import org.testng.annotations.Test;
+import pages.SignUpPage;
 
 public class SignUpTest {
     @Test
     public void signUp(){
         HomePage homePage = new HomePage();
         homePage.clickOnSignUp();
+        SignUpPage signUpPage = new SignUpPage();
+        signUpPage.typeUsername();
+        signUpPage.typeFullname();
+        signUpPage.typeEmail();
+        signUpPage.clickOnCheck();
+        signUpPage.clickOnSignUop();
+        assertTrue(ge)
     }
 }
